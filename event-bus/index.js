e3d97dd36e642727d3fd4cfbb36be96b3f0c579f
Connor Keating <cjkeati1@asu.edu>
2020-12-11 16:43:21 -0700

    Events are now stored so query service can sync up if it missed anything

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,16 +5,25 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events', (async (req, res) => {
+const events = [];
+
+app.post('/events', async (req, res) => {
    const event = req.body;
+
+   events.push(event);
 
    await axios.post('http://localhost:4000/events', event);
    await axios.post('http://localhost:4001/events', event);
    await axios.post('http://localhost:4002/events', event);
+   await axios.post('http://localhost:4003/events', event);
 
-   res.status(200).send("OK!");
-}))
+   res.send({status: 'OK'});
+});
+
+app.get('/events', (req, res) => {
+   res.send(events);
+});
 
 app.listen(4005, () => {
-   console.log("Listening on 4005");
-})
+   console.log('Listening on 4005');
+});
