bf14f75ce9312a4e3f04fb6b94901573226fb880
Connor Keating <cjkeati1@asu.edu>
2020-12-14 22:40:14 -0700

    Created depls and k8 services for all apps

--- posts/index.js
+++ posts/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {randomBytes} = require('crypto');
+const { randomBytes } = require('crypto');
 const cors = require('cors');
 const axios = require('axios');
 
@@ -11,36 +11,36 @@ app.use(cors());
 const posts = {};
 
 app.get('/posts', (req, res) => {
-   res.send(posts);
+  res.send(posts);
 });
 
 app.post('/posts', async (req, res) => {
-   const id = randomBytes(4).toString('hex');
-   const {title} = req.body;
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
 
-   posts[id] = {
+  posts[id] = {
+    id,
+    title
+  };
+
+  await axios.post('http://event-bus-srv:4005/events', {
+    type: 'PostCreated',
+    data: {
       id,
       title
-   };
+    }
+  });
 
-   await axios.post('http://event-bus-srv:4005/events',
-      {
-         type: "PostCreated",
-         data: {
-            id, title
-         }
-      });
-
-   res.status(201).send(posts[id]);
+  res.status(201).send(posts[id]);
 });
 
-app.post('/events', ((req, res) => {
-   console.log("Received Event", req.body.type);
+app.post('/events', (req, res) => {
+  console.log('Received Event', req.body.type);
 
-   res.status(200).send("Got it");
-}))
+  res.send({});
+});
 
 app.listen(4000, () => {
-   console.log('v55');
-   console.log('Listening on 4000');
+  console.log('v55');
+  console.log('Listening on 4000');
 });
