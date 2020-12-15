22fbbf7ffb1cc7d55e4c5a90185338c08f89b45f
Connor Keating <cjkeati1@asu.edu>
2020-12-14 22:58:40 -0700

    misc

--- event-bus/index.js
+++ event-bus/index.js
@@ -7,23 +7,23 @@ app.use(bodyParser.json());
 
 const events = [];
 
-app.post('/events', (req, res) => {
-  const event = req.body;
+app.post('/events', async (req, res) => {
+   const event = req.body;
 
-  events.push(event);
+   events.push(event);
 
-  axios.post('http://posts-clusterip-srv:4000/events', event);
-  axios.post('http://comments-srv:4001/events', event);
-  axios.post('http://query-srv:4002/events', event);
-  axios.post('http://moderation-srv:4003/events', event);
+   await axios.post('http://posts-clusterip-srv:4000/events', event);
+   await axios.post('http://comments-srv:4001/events', event);
+   await axios.post('http://query-srv:4002/events', event);
+   await axios.post('http://moderation-srv:4003/events', event);
 
-  res.send({ status: 'OK' });
+   res.send({status: 'OK'});
 });
 
 app.get('/events', (req, res) => {
-  res.send(events);
+   res.send(events);
 });
 
 app.listen(4005, () => {
-  console.log('Listening on 4005');
+   console.log('Listening on 4005');
 });
